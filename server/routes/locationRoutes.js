6d7c7ae5f3d4ba05f6763defe2bc7c9c24d1545f
Harsh sharma <harsh29012006@gmail.com>
2026-06-26 19:51:25 +0530

    edit authmiddleware

--- server/routes/locationRoutes.js
+++ server/routes/locationRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const authMiddleware = require("../../middleware/authMiddleware");
+const authMiddleware = require("../middleware/authMiddleware");
 const routes = express.Router();
 
 routes.post("/current", authMiddleware, async(req, res) => {
